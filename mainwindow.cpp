f79ce36688ddf13b93f0aa21accc4d5b8873ce88
Ilya Ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-09-23 23:31:08 +0300

    Added checking keys and encrypted filename

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,6 +20,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void printMessage(QString str) {
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("Информационное сообщение");
+    msgBox.setText(str);
+    msgBox.setIcon(QMessageBox::Information); // Установите иконку
+    msgBox.setStandardButtons(QMessageBox::Ok); // Кнопка "ОК"
+    msgBox.exec(); // Показать сообщение
+}
+
 void MainWindow::on_btn_crypt_clicked()
 {
     QString filePath = ui->lineEdit_choose->text();
@@ -27,35 +36,43 @@ void MainWindow::on_btn_crypt_clicked()
 
     ifstream ReadStream(file_to_encrypt);
 
-    string str;
-
     QString lineEdit_first_key = ui->lineEdit_first_key->text();
     QString lineEdit_second_key = ui->lineEdit_second_key->text();
 
-    while (getline(ReadStream, str))
-    {
-        ofstream WriteStream("encrypted_text.txt");//открываем поток на запись
+    bool check_first_key;
+    bool check_second_key;
 
-        int lenght = str.length();
+    int first_key = lineEdit_first_key.toInt(&check_first_key, 10);
+    int second_key = lineEdit_second_key.toInt(&check_second_key, 10);
 
-        //цикл посимвольного шифрования
-        for (int i = 0; i < lenght; i++)
+    string str;
+
+    if (check_first_key == false && check_second_key == false) {
+        printMessage("Вы неверно ввели оба ключа.\nПерепроверьте ключи.");
+    } else if (check_first_key == false && check_second_key == true) {
+        printMessage("Вы неверно ввели первый ключ.\nПерепроверьте ключ.");
+    } else if (check_first_key == true && check_second_key == false) {
+        printMessage("Вы неверно ввели второй ключ.\nПерепроверьте ключ.");
+    } else {
+        while (getline(ReadStream, str))
         {
-            unsigned char enc = static_cast<unsigned char>(str[i]);
-            enc += lineEdit_first_key.toInt();
-            enc += lineEdit_second_key.toInt();
-            WriteStream << enc;
+            ofstream WriteStream("encrypted_text.txt");//открываем поток на запись
+
+            int lenght = str.length();
+
+            //цикл посимвольного шифрования
+            for (int i = 0; i < lenght; i++)
+            {
+                unsigned char enc = static_cast<unsigned char>(str[i]);
+                enc += first_key;
+                enc += second_key;
+                WriteStream << enc;
+            }
+
+            WriteStream.close();//закрываем поток на запись
         }
-
-        WriteStream.close();//закрываем поток на запись
+        printMessage("Текст из выбранного файла\nзашифрован и сохранён в файл encrypted_text.txt");
     }
-
-    QMessageBox msgBox;
-    msgBox.setWindowTitle("Информационное сообщение");
-    msgBox.setText("Текст из выбранного файла\nзашифрован и сохранён в файл encrypted_text.txt");
-    msgBox.setIcon(QMessageBox::Information); // Установите иконку
-    msgBox.setStandardButtons(QMessageBox::Ok); // Кнопка "ОК"
-    msgBox.exec(); // Показать сообщение
 }
 
 
@@ -68,41 +85,56 @@ void MainWindow::on_btn_choose_clicked()
 
 void MainWindow::on_btn_decrypt_clicked()
 {
+    QString lineEdit_first_key = ui->lineEdit_first_key->text();
+    QString lineEdit_second_key = ui->lineEdit_second_key->text();
+
+    bool check_first_key;
+    bool check_second_key;
+
+    int first_key = lineEdit_first_key.toInt(&check_first_key, 10);
+    int second_key = lineEdit_second_key.toInt(&check_second_key, 10);
+
     QString filePath = ui->lineEdit_choose->text();
     const char* file_to_decrypt = filePath.toUtf8().constData();
 
-    ifstream ReadStream(file_to_decrypt);
+    filesystem::path p(file_to_decrypt);
 
-    if (ReadStream.is_open())
-    {
-        string str;
+    if (p.filename() != "encrypted_text.txt") {
+        printMessage("Вы выбрали неверный файл.\nВыберите зашифрованный файл.");
+    } else {
+        if (check_first_key == false && check_second_key == false) {
+            printMessage("Вы неверно ввели оба ключа.\nПерепроверьте ключи.");
+        } else if (check_first_key == false && check_second_key == true) {
+            printMessage("Вы неверно ввели первый ключ.\nПерепроверьте ключ.");
+        } else if (check_first_key == true && check_second_key == false) {
+            printMessage("Вы неверно ввели второй ключ.\nПерепроверьте ключ.");
+        } else {
+            ifstream ReadStream(file_to_decrypt);
 
-        QString lineEdit_first_key = ui->lineEdit_first_key->text();
-        QString lineEdit_second_key = ui->lineEdit_second_key->text();
-
-        while (getline(ReadStream, str))
-        {
-            ofstream WriteStream("decrypted_text.txt");//открываем поток на запись
-            int lenght = str.length();
-
-            //цикл посимвольного шифрования
-            for (int i = 0; i < lenght; i++)
+            if (ReadStream.is_open())
             {
-                unsigned char dec = static_cast<unsigned char>(str[i]);
-                dec -= lineEdit_first_key.toInt();
-                dec -= lineEdit_second_key.toInt();
-                WriteStream << dec;
+                string str;
+
+                while (getline(ReadStream, str))
+                {
+                    ofstream WriteStream("decrypted_text.txt");//открываем поток на запись
+                    int lenght = str.length();
+
+                    //цикл посимвольного шифрования
+                    for (int i = 0; i < lenght; i++)
+                    {
+                        unsigned char dec = static_cast<unsigned char>(str[i]);
+                        dec -= first_key;
+                        dec -= second_key;
+                        WriteStream << dec;
+                    }
+
+                    WriteStream.close();//закрываем поток на запись
+                }
+
             }
-
-            WriteStream.close();//закрываем поток на запись
+            printMessage("Текст из выбранного файла\nрасшифрован и сохранён в файл decrypted_text.txt");
         }
-
     }
-    QMessageBox msgBox;
-    msgBox.setWindowTitle("Информационное сообщение");
-    msgBox.setText("Текст из выбранного файла\nрасшифрован и сохранён в файл decrypted_text.txt");
-    msgBox.setIcon(QMessageBox::Information); // Установите иконку
-    msgBox.setStandardButtons(QMessageBox::Ok); // Кнопка "ОК"
-    msgBox.exec(); // Показать сообщение
 }
 
