b913088ead2f578feafbd32e4fa164db30aca25c
Ilya Ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-10-02 18:43:51 +0300

    Deleted presentation

---
+++ build/Qt_Work_6_7_2-Release/ui_mainwindow.h
@@ -0,0 +1,159 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *btn_crypt;
+    QPushButton *btn_decrypt;
+    QWidget *horizontalLayoutWidget_2;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_choose;
+    QLineEdit *lineEdit_choose;
+    QPushButton *btn_choose;
+    QWidget *horizontalLayoutWidget_3;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_first_key;
+    QLineEdit *lineEdit_first_key;
+    QWidget *horizontalLayoutWidget_4;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_second_key;
+    QLineEdit *lineEdit_second_key;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(488, 299);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        horizontalLayoutWidget = new QWidget(centralwidget);
+        horizontalLayoutWidget->setObjectName("horizontalLayoutWidget");
+        horizontalLayoutWidget->setGeometry(QRect(50, 220, 391, 41));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName("horizontalLayout");
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        btn_crypt = new QPushButton(horizontalLayoutWidget);
+        btn_crypt->setObjectName("btn_crypt");
+
+        horizontalLayout->addWidget(btn_crypt);
+
+        btn_decrypt = new QPushButton(horizontalLayoutWidget);
+        btn_decrypt->setObjectName("btn_decrypt");
+
+        horizontalLayout->addWidget(btn_decrypt);
+
+        horizontalLayoutWidget_2 = new QWidget(centralwidget);
+        horizontalLayoutWidget_2->setObjectName("horizontalLayoutWidget_2");
+        horizontalLayoutWidget_2->setGeometry(QRect(50, 30, 381, 51));
+        horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget_2);
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label_choose = new QLabel(horizontalLayoutWidget_2);
+        label_choose->setObjectName("label_choose");
+
+        horizontalLayout_2->addWidget(label_choose);
+
+        lineEdit_choose = new QLineEdit(horizontalLayoutWidget_2);
+        lineEdit_choose->setObjectName("lineEdit_choose");
+
+        horizontalLayout_2->addWidget(lineEdit_choose);
+
+        btn_choose = new QPushButton(horizontalLayoutWidget_2);
+        btn_choose->setObjectName("btn_choose");
+
+        horizontalLayout_2->addWidget(btn_choose);
+
+        horizontalLayoutWidget_3 = new QWidget(centralwidget);
+        horizontalLayoutWidget_3->setObjectName("horizontalLayoutWidget_3");
+        horizontalLayoutWidget_3->setGeometry(QRect(70, 90, 341, 51));
+        horizontalLayout_3 = new QHBoxLayout(horizontalLayoutWidget_3);
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        horizontalLayout_3->setContentsMargins(0, 0, 0, 0);
+        label_first_key = new QLabel(horizontalLayoutWidget_3);
+        label_first_key->setObjectName("label_first_key");
+
+        horizontalLayout_3->addWidget(label_first_key);
+
+        lineEdit_first_key = new QLineEdit(horizontalLayoutWidget_3);
+        lineEdit_first_key->setObjectName("lineEdit_first_key");
+
+        horizontalLayout_3->addWidget(lineEdit_first_key);
+
+        horizontalLayoutWidget_4 = new QWidget(centralwidget);
+        horizontalLayoutWidget_4->setObjectName("horizontalLayoutWidget_4");
+        horizontalLayoutWidget_4->setGeometry(QRect(70, 150, 341, 51));
+        horizontalLayout_4 = new QHBoxLayout(horizontalLayoutWidget_4);
+        horizontalLayout_4->setObjectName("horizontalLayout_4");
+        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
+        label_second_key = new QLabel(horizontalLayoutWidget_4);
+        label_second_key->setObjectName("label_second_key");
+
+        horizontalLayout_4->addWidget(label_second_key);
+
+        lineEdit_second_key = new QLineEdit(horizontalLayoutWidget_4);
+        lineEdit_second_key->setObjectName("lineEdit_second_key");
+
+        horizontalLayout_4->addWidget(lineEdit_second_key);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 488, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        btn_crypt->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\321\210\320\270\321\204\321\200\320\276\320\262\320\260\321\202\321\214 \320\264\320\260\320\275\320\275\321\213\320\265", nullptr));
+        btn_decrypt->setText(QCoreApplication::translate("MainWindow", "\320\224\320\265\321\210\320\270\321\204\321\200\320\276\320\262\320\260\321\202\321\214 \320\264\320\260\320\275\320\275\321\213\320\265", nullptr));
+        label_choose->setText(QCoreApplication::translate("MainWindow", "\320\222\321\213\320\261\320\265\321\200\320\270\321\202\320\265 \321\204\320\260\320\271\320\273", nullptr));
+        btn_choose->setText(QCoreApplication::translate("MainWindow", "\320\236\320\261\320\267\320\276\321\200", nullptr));
+        label_first_key->setText(QCoreApplication::translate("MainWindow", "1 \320\272\320\273\321\216\321\207", nullptr));
+        lineEdit_first_key->setText(QString());
+        label_second_key->setText(QCoreApplication::translate("MainWindow", "2 \320\272\320\273\321\216\321\207", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
