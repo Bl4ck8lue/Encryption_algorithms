fd96723efbb2c25a304fe9a923823d68e1e8be96
Ilya Ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-10-07 22:51:34 +0300

    Edited main program. Deleted keys and changed algorithm

--- build/Qt_Work_6_7_2-Release/ui_mainwindow.h
+++ build/Qt_Work_6_7_2-Release/ui_mainwindow.h
@@ -35,14 +35,6 @@ public:
     QLabel *label_choose;
     QLineEdit *lineEdit_choose;
     QPushButton *btn_choose;
-    QWidget *horizontalLayoutWidget_3;
-    QHBoxLayout *horizontalLayout_3;
-    QLabel *label_first_key;
-    QLineEdit *lineEdit_first_key;
-    QWidget *horizontalLayoutWidget_4;
-    QHBoxLayout *horizontalLayout_4;
-    QLabel *label_second_key;
-    QLineEdit *lineEdit_second_key;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -50,12 +42,12 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(488, 299);
+        MainWindow->resize(478, 218);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         horizontalLayoutWidget = new QWidget(centralwidget);
         horizontalLayoutWidget->setObjectName("horizontalLayoutWidget");
-        horizontalLayoutWidget->setGeometry(QRect(50, 220, 391, 41));
+        horizontalLayoutWidget->setGeometry(QRect(50, 110, 391, 41));
         horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
         horizontalLayout->setObjectName("horizontalLayout");
         horizontalLayout->setContentsMargins(0, 0, 0, 0);
@@ -90,42 +82,10 @@ public:
 
         horizontalLayout_2->addWidget(btn_choose);
 
-        horizontalLayoutWidget_3 = new QWidget(centralwidget);
-        horizontalLayoutWidget_3->setObjectName("horizontalLayoutWidget_3");
-        horizontalLayoutWidget_3->setGeometry(QRect(70, 90, 341, 51));
-        horizontalLayout_3 = new QHBoxLayout(horizontalLayoutWidget_3);
-        horizontalLayout_3->setObjectName("horizontalLayout_3");
-        horizontalLayout_3->setContentsMargins(0, 0, 0, 0);
-        label_first_key = new QLabel(horizontalLayoutWidget_3);
-        label_first_key->setObjectName("label_first_key");
-
-        horizontalLayout_3->addWidget(label_first_key);
-
-        lineEdit_first_key = new QLineEdit(horizontalLayoutWidget_3);
-        lineEdit_first_key->setObjectName("lineEdit_first_key");
-
-        horizontalLayout_3->addWidget(lineEdit_first_key);
-
-        horizontalLayoutWidget_4 = new QWidget(centralwidget);
-        horizontalLayoutWidget_4->setObjectName("horizontalLayoutWidget_4");
-        horizontalLayoutWidget_4->setGeometry(QRect(70, 150, 341, 51));
-        horizontalLayout_4 = new QHBoxLayout(horizontalLayoutWidget_4);
-        horizontalLayout_4->setObjectName("horizontalLayout_4");
-        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
-        label_second_key = new QLabel(horizontalLayoutWidget_4);
-        label_second_key->setObjectName("label_second_key");
-
-        horizontalLayout_4->addWidget(label_second_key);
-
-        lineEdit_second_key = new QLineEdit(horizontalLayoutWidget_4);
-        lineEdit_second_key->setObjectName("lineEdit_second_key");
-
-        horizontalLayout_4->addWidget(lineEdit_second_key);
-
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 488, 22));
+        menubar->setGeometry(QRect(0, 0, 478, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -143,9 +103,6 @@ public:
         btn_decrypt->setText(QCoreApplication::translate("MainWindow", "\320\224\320\265\321\210\320\270\321\204\321\200\320\276\320\262\320\260\321\202\321\214 \320\264\320\260\320\275\320\275\321\213\320\265", nullptr));
         label_choose->setText(QCoreApplication::translate("MainWindow", "\320\222\321\213\320\261\320\265\321\200\320\270\321\202\320\265 \321\204\320\260\320\271\320\273", nullptr));
         btn_choose->setText(QCoreApplication::translate("MainWindow", "\320\236\320\261\320\267\320\276\321\200", nullptr));
-        label_first_key->setText(QCoreApplication::translate("MainWindow", "1 \320\272\320\273\321\216\321\207", nullptr));
-        lineEdit_first_key->setText(QString());
-        label_second_key->setText(QCoreApplication::translate("MainWindow", "2 \320\272\320\273\321\216\321\207", nullptr));
     } // retranslateUi
 
 };
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,6 +20,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
 void printMessage(QString str) {
     QMessageBox msgBox;
     msgBox.setWindowTitle("Информационное сообщение");
@@ -29,50 +30,108 @@ void printMessage(QString str) {
     msgBox.exec(); // Показать сообщение
 }
 
+void Encrypt_Decrypt(const char* file_to_encrypt, string &str, int &user_choice)
+{
+    ofstream WriteStream;//открываем поток на запись
+    WriteStream.open("encrypted_text.txt", ios::app);//для шифровки, поток открыт на дозапись
+    if (user_choice == 2)
+    {
+        WriteStream.close();
+        WriteStream.open("decrypted_text.txt", ios::app);//для расшифровки
+    }
+    //проверяем количество слов в текте файла
+    const string separators{ " \n" }; //строка разделителей
+    vector <string> words; //вектор в котором будут храниться слова из текста
+    size_t start{ str.find_first_not_of(separators) }; // начальный индекс первого слова
+    while (start != string::npos) // проходим, пока в строке не окажется других символов, кроме separators
+    {
+        size_t end = str.find_first_of(separators, start + 1); // находим, где кончается слово
+        if (end == string::npos) // если НЕ найден ни один из символов-разделителей
+        {
+            end = str.length();        // устанавливаем переменную end на конец текста
+        }
+        words.push_back(str.substr(start, end - start)); // добавляем в вектор слово
+        start = str.find_first_not_of(separators, end + 1); // находим начальный индекс следующего слова и переустанавливаем start
+    }
+    int vector_length = words.size();//длина вектора
+    //перемещение по словам в векторе
+    for (int j = 0; j < vector_length; j++)
+    {
+        if (j > 0)
+        {
+            WriteStream << " ";//запись пробелов в текст после первого слова
+        }
+        int word_length = words[j].length();//длина слова
+        //цикл посимвольного шифрования
+        for (int i = 0; i < word_length; i++)
+        {
+            unsigned char enc = static_cast<unsigned char>(words[j][i]);//определяем символ для шифрования
+            if (user_choice == 1)
+            {
+                enc += j + 1;//определяем ключ шифрования в данном слове и шифруем
+            }
+            else
+            {
+                enc -= j + 1;//определяем ключ шифрования в данном слове и дешифруем
+            }
+            WriteStream << enc;//запись в файл
+        }
+        if (j == vector_length - 1)
+        {
+            WriteStream << "\n";//если слово является концом строки, то мы переносим строку
+        }
+    }
+    if (user_choice == 1) {
+        printMessage("Текст из выбранного файла\nзашифрован и сохранён в файл encrypted_text.txt");
+    } else {
+        printMessage("Текст из выбранного файла\nрасшифрован и сохранён в файл decrypted_text.txt");
+    }
+    WriteStream.close();//закрываем поток на запись
+}
+
+
+void ReadStringFromFile(const char* file_to_encrypt, int &user_choice)
+{
+    string str;
+    //открываем поток на чтение из выбранного файла
+    ifstream ReadStream;
+    ReadStream.open(file_to_encrypt, ios::in);//чтение построчно
+
+    if (user_choice == 2)
+    {
+        ReadStream.close();
+        ReadStream.open(file_to_encrypt, ios::in);
+    }
+
+    //проверяем открыт ли поток на чтение, если да, то читаем файл построчно и передаём эти строки в функцию шифрования
+    //если нет, то так и говорим, в интерфейсе там будет message box ну или что-то типа него
+    if (ReadStream.is_open())
+    {
+        //int encrypt_key;
+        while (getline(ReadStream, str))
+        {
+            Encrypt_Decrypt(file_to_encrypt, str, user_choice);
+        }
+    }
+    else
+    {
+        printMessage("Не выбран файл.");
+    }
+    ReadStream.close();
+    exit;
+}
+
 void MainWindow::on_btn_crypt_clicked()
 {
     QString filePath = ui->lineEdit_choose->text();
     const char* file_to_encrypt = filePath.toUtf8().constData();
 
-    ifstream ReadStream(file_to_encrypt);
+    int user_choice = 1;
 
-    QString lineEdit_first_key = ui->lineEdit_first_key->text();
-    QString lineEdit_second_key = ui->lineEdit_second_key->text();
+    ofstream out("encrypted_text.txt", ios::out);//очистка шифруемого файла
+    out.close();
 
-    bool check_first_key;
-    bool check_second_key;
-
-    int first_key = lineEdit_first_key.toInt(&check_first_key, 10);
-    int second_key = lineEdit_second_key.toInt(&check_second_key, 10);
-
-    string str;
-
-    if (check_first_key == false && check_second_key == false) {
-        printMessage("Вы неверно ввели оба ключа.\nПерепроверьте ключи.");
-    } else if (check_first_key == false && check_second_key == true) {
-        printMessage("Вы неверно ввели первый ключ.\nПерепроверьте ключ.");
-    } else if (check_first_key == true && check_second_key == false) {
-        printMessage("Вы неверно ввели второй ключ.\nПерепроверьте ключ.");
-    } else {
-        while (getline(ReadStream, str))
-        {
-            ofstream WriteStream("encrypted_text.txt");//открываем поток на запись
-
-            int lenght = str.length();
-
-            //цикл посимвольного шифрования
-            for (int i = 0; i < lenght; i++)
-            {
-                unsigned char enc = static_cast<unsigned char>(str[i]);
-                enc += first_key;
-                enc += second_key;
-                WriteStream << enc;
-            }
-
-            WriteStream.close();//закрываем поток на запись
-        }
-        printMessage("Текст из выбранного файла\nзашифрован и сохранён в файл encrypted_text.txt");
-    }
+    ReadStringFromFile(file_to_encrypt, user_choice);
 }
 
 
@@ -85,56 +144,13 @@ void MainWindow::on_btn_choose_clicked()
 
 void MainWindow::on_btn_decrypt_clicked()
 {
-    QString lineEdit_first_key = ui->lineEdit_first_key->text();
-    QString lineEdit_second_key = ui->lineEdit_second_key->text();
-
-    bool check_first_key;
-    bool check_second_key;
-
-    int first_key = lineEdit_first_key.toInt(&check_first_key, 10);
-    int second_key = lineEdit_second_key.toInt(&check_second_key, 10);
-
     QString filePath = ui->lineEdit_choose->text();
-    const char* file_to_decrypt = filePath.toUtf8().constData();
+    const char* file_to_encrypt = filePath.toUtf8().constData();
 
-    filesystem::path p(file_to_decrypt);
+    int user_choice = 2;
+    ofstream out2("decrypted_text.txt", ios::out);//очистка расшифрованного файла
+    out2.close();
 
-    if (p.filename() != "encrypted_text.txt") {
-        printMessage("Вы выбрали неверный файл.\nВыберите зашифрованный файл.");
-    } else {
-        if (check_first_key == false && check_second_key == false) {
-            printMessage("Вы неверно ввели оба ключа.\nПерепроверьте ключи.");
-        } else if (check_first_key == false && check_second_key == true) {
-            printMessage("Вы неверно ввели первый ключ.\nПерепроверьте ключ.");
-        } else if (check_first_key == true && check_second_key == false) {
-            printMessage("Вы неверно ввели второй ключ.\nПерепроверьте ключ.");
-        } else {
-            ifstream ReadStream(file_to_decrypt);
-
-            if (ReadStream.is_open())
-            {
-                string str;
-
-                while (getline(ReadStream, str))
-                {
-                    ofstream WriteStream("decrypted_text.txt");//открываем поток на запись
-                    int lenght = str.length();
-
-                    //цикл посимвольного шифрования
-                    for (int i = 0; i < lenght; i++)
-                    {
-                        unsigned char dec = static_cast<unsigned char>(str[i]);
-                        dec -= first_key;
-                        dec -= second_key;
-                        WriteStream << dec;
-                    }
-
-                    WriteStream.close();//закрываем поток на запись
-                }
-
-            }
-            printMessage("Текст из выбранного файла\nрасшифрован и сохранён в файл decrypted_text.txt");
-        }
-    }
+   ReadStringFromFile(file_to_encrypt, user_choice);
 }
 
